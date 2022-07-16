bdded923e74be3145f5f7b80285b709a1c8773d2
Gleyson Sampaio <glysns@hotmail.com>
2022-07-16 18:09:47 -0300

    criando o dtos

--- src/main/java/digytal/hibernate/tuple/converter/model/dto/MarcaDto.java
+++ src/main/java/digytal/hibernate/tuple/converter/model/dto/MarcaDto.java
@@ -1,4 +1,22 @@
 package digytal.hibernate.tuple.converter.model.dto;
 
 public class MarcaDto {
+    private Integer id;
+    private String nome;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
